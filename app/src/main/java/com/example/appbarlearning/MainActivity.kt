5650058697cc75de8948d6e87fd3f25973634f3a
AndreyG <nba.grinin.96@gmail.com>
2022-01-03 18:32:20 +0700

    add listener on menu item

--- app/src/main/java/com/example/appbarlearning/MainActivity.kt
+++ app/src/main/java/com/example/appbarlearning/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.appbarlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.activity_test_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        Toast.makeText(this, "You click on menu item", Toast.LENGTH_LONG).show()
+        return true
     }
 }
